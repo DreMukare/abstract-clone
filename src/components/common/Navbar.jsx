ab51a4df277555f825e4ad0b59ba9623966ad7ef
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-11-08 17:36:58 +0300

    feat(nav): created and styled navbar

--- src/components/common/Navbar.jsx
+++ src/components/common/Navbar.jsx
@@ -1,7 +1,54 @@
-import React from "react";
+import Logo from "./Logo";
+import Button from "./Button";
+import theme from "../../utils/theme";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav
+      style={{
+        background: theme.colors.black,
+        color: theme.colors.white,
+        width: "100vw",
+        minHeight: "10vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        padding: "0 2em",
+      }}
+    >
+      <NavBarButtonSection>
+        <Logo />
+        <div
+          style={{
+            minHeight: "100%",
+            height: "100%",
+            borderLeft: `1px solid ${theme.colors.white}`,
+            paddingTop: "20px",
+          }}
+        ></div>
+        <a href="#">Help Center</a>
+      </NavBarButtonSection>
+      <NavBarButtonSection>
+        <Button buttonText="Submit a request" handleClick={() => {}} />
+        <Button buttonText="Sign in" handleClick={() => {}} variant="filled" />
+      </NavBarButtonSection>
+    </nav>
+  );
+};
+
+const NavBarButtonSection = (props) => {
+  const { children } = props;
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        gap: "8px",
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Navbar;
