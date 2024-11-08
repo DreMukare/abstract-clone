6a6ee131528a6d2061a2c180e9f270cf72f2906d
Andrew Mukare Ngumbao <dremukare@gmail.com>
2024-11-08 17:49:05 +0300

    feat(update): using new layout component

--- src/components/pages/LandingPage.jsx
+++ src/components/pages/LandingPage.jsx
@@ -1,5 +1,23 @@
+import theme from "../../utils/theme";
+import Hero from "../common/Hero";
+import Layout from "../layout/Layout";
+
 const LandingPage = () => {
-  return <div>LandingPage</div>;
+  return (
+    <Layout
+      heroContent={
+        <div
+          style={{
+            textAlign: "center",
+          }}
+        >
+          <h1>How can we help?</h1>
+          <p>search</p>
+        </div>
+      }
+      pageContent={<p>Coming soon</p>}
+    />
+  );
 };
 
 export default LandingPage;
